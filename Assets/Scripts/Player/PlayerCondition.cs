03edbd0e3329eb439d2bf083f0c3537beb0c8d8e
kierwl <45115106+kierwl@users.noreply.github.com>
2025-03-04 20:22:09 +0900

    feat: 캠프파이어 데미지 까지 구현

---
+++ Assets/Scripts/Player/PlayerCondition.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEngine;
+
+public interface IDamageable
+{
+    void TakePhysicalDamage(int damageAmount);
+}
+public class PlayerCondition : MonoBehaviour ,IDamageable
+{
+    public UICondition uiCondition;
+
+    Condition health { get { return uiCondition.health; } }
+    Condition hunger { get { return uiCondition.hunger; } }
+    Condition stamina { get { return uiCondition.stamina; } }
+
+    public float noHungerHealthDecay;
+    public event Action onTakeDamage;
+
+    private void Update()
+    {
+        hunger.Subtract(hunger.passiveValue * Time.deltaTime);
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if (hunger.curValue <= 0f)
+        {
+            health.Subtract(noHungerHealthDecay * Time.deltaTime);
+            Debug.Log("허기로 인한 체력 감소");
+        }
+
+        if (health.curValue < 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void Eat(float amount)
+    {
+        hunger.Add(amount);
+    }
+
+    public void Die()
+    {
+        Debug.Log("플레이어가 죽었다.");
+    }
+
+    public void TakePhysicalDamage(int damageAmount)
+    {
+        health.Subtract(damageAmount);
+        onTakeDamage?.Invoke();
+    }
+}
